5b831f455eb673cf30d75f51e6f54854aa8d0453
Marshmallow1119 <tomasbras@ua.pt>
2024-04-18 15:44:01 +0100

    .

--- Aula03/Visitor.java
+++ Aula03/Visitor.java
@@ -1,26 +1,26 @@
 @SuppressWarnings("CheckReturnValue")
 public class Visitor extends CSVBaseVisitor<Object> {
-   private static StringBuilder html = new StringBuilder();
-    
+    private static StringBuilder html = new StringBuilder();
+
     @Override
     public Void visitRow(CSVParser.RowContext ctx) {
-        html.append("<tr>\n");
+        html.append("    <tr>\n");
         for (CSVParser.FieldContext field : ctx.field()) {
-            html.append(" <td>").append(field.TEXT().getText()).append("</td>\n");
+            html.append("        <td>").append(field.TEXT().getText()).append("</td>\n");
         }
-        html.append("</tr>\n");
+        html.append("    </tr>\n");
         return null;
     }
-    
+
     public static void startTable() {
         html.append("<table>\n");
     }
 
-    // Method to finalize the table
+    // Método para finalizar a tabela
     public static void endTable() {
         html.append("</table>\n");
     }
-    
+
     public static String getHtml() {
         return html.toString();
     }
